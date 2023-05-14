eeb4ee7374e76ae593ab360d86683e90fabd2887
Chhakuli <chhakulizingare12@gmail.com>
2023-05-14 20:49:22 +0530

    feat: Add single-point export for MUI icons

---
+++ src/assets/icons/materialIcons.js
@@ -0,0 +1,11 @@
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+
+export {
+  SearchIcon,
+  PersonOutlineOutlinedIcon,
+  FavoriteBorderOutlinedIcon,
+  ShoppingBagOutlinedIcon,
+};

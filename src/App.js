849b76014ab707cdd3890366fe1088eb0a9affd8
Chhakuli <chhakulizingare12@gmail.com>
2023-05-14 20:48:12 +0530

    feat: Add Navbar and Routes configuration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
+import "./App.css";
+import { HomePage } from "pages";
+import { Navbar } from "components";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

5d3be44d903d693d24ac839e066419f382672d05
Chhakuli <chhakulizingare12@gmail.com>
2023-05-14 20:48:27 +0530

    feat: Wrap App component with Router in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
@@ -9,7 +11,9 @@ makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );

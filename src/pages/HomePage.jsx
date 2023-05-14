da6293c51b8c6d6669b2e4bdf8a7ce1cb3372f80
Chhakuli <chhakulizingare12@gmail.com>
2023-05-14 20:51:37 +0530

    feat: Add Homepage component

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import { Carousel, Categories } from "components";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Carousel />
+      <Categories />
+    </div>
+  );
+};
+
+export { HomePage };

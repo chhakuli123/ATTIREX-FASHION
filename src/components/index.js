afeae5bde0e35d88ae568990a5ac07c2a473b3a9
Chhakuli <chhakulizingare12@gmail.com>
2023-05-15 15:41:45 +0530

    feat: Export new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { Navbar } from "./navbar/Navbar";
 export { Carousel } from "./carousel/Carousel";
 export { Categories } from "./categories/Categories";
+export { SaleBanner } from "./sale/SaleBanner";
+export { ClothingCategories } from "./clothing-categories/ClothingCategories";

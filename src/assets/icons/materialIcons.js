54cc5a1e51346aec664fed4e93c65229dad6d6c7
Chhakuli <chhakulizingare12@gmail.com>
2023-05-15 15:39:32 +0530

    feat: Add icons for Footer

--- src/assets/icons/materialIcons.js
+++ src/assets/icons/materialIcons.js
@@ -2,10 +2,17 @@ import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
-
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 export {
   SearchIcon,
   PersonOutlineOutlinedIcon,
   FavoriteBorderOutlinedIcon,
   ShoppingBagOutlinedIcon,
+  GitHubIcon,
+  TwitterIcon,
+  InstagramIcon,
+  LinkedInIcon,
 };

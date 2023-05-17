2dd86663009f4ae7ca84f37fed3a3bbcd3b7bb42
Chhakuli <chhakulizingare12@gmail.com>
2023-05-17 19:02:31 +0530

    feat: Add path for ProductsPage

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "pages";
 import { Footer, Navbar } from "components";
+import ProductsPage from "pages/ProductsPage";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
       </Routes>
       <Footer />
     </div>

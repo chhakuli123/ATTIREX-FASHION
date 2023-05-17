d1fed54ac2b271422dd9ceac9c9c3a1c248dd57d
Chhakuli <chhakulizingare12@gmail.com>
2023-05-17 17:49:23 +0530

    feat: Add action dispatch and navigate to products

--- src/components/sale/SaleBanner.jsx
+++ src/components/sale/SaleBanner.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./salebanner.css";
+import { useNavigate } from "react-router-dom";
+import { useProductsData } from "context";
 
 const SaleBanner = () => {
+  const {dispatch}=useProductsData()
+  const navigate=useNavigate()
   return (
     <div class="sale">
       <div class="sale-title">
@@ -12,14 +16,26 @@ const SaleBanner = () => {
         <img
           src="https://res.cloudinary.com/dptfwcnro/image/upload/v1683903022/E-comm%20ATTIREX/Sale-Banner-1_mzj7ko.gif"
           alt="Sale-Banner-1"
+          onClick={() => {
+            dispatch({ type: "CLEAR_FILTERS" });
+            navigate("/products");
+          }}
         />
         <img
           src="https://res.cloudinary.com/dptfwcnro/image/upload/v1683903949/E-comm%20ATTIREX/Sale-Banner-2_rifmbx.jpg"
           alt="Sale-Banner-2"
+          onClick={() => {
+            dispatch({ type: "CLEAR_FILTERS" });
+            navigate("/products");
+          }}
         />
         <img
           src="https://res.cloudinary.com/dptfwcnro/image/upload/v1683903950/E-comm%20ATTIREX/Sale-Banner-3_si788m.jpg"
           alt="Sale-Banner-3"
+          onClick={() => {
+            dispatch({ type: "CLEAR_FILTERS" });
+            navigate("/products");
+          }}
         />
       </div>
     </div>

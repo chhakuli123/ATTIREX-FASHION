af5ec1dcedffe658c4190517c8c3a7bfd4a2d9da
Chhakuli <chhakulizingare12@gmail.com>
2023-05-18 15:36:48 +0530

    chore: Minor export updates

---
+++ src/utils/index.js
@@ -0,0 +1,20 @@
+export {
+  searchProducts,
+  updateProducts,
+  sortProducts,
+  updatePriceRange,
+  sortByRating,
+  filterBySection,
+  filterByCategory,
+  clearFilters,
+} from "./filterActions";
+export {
+  SEARCH_PRODUCTS,
+  UPDATE_PRODUCTS,
+  SORT_PRODUCTS,
+  UPDATE_PRICE_RANGE,
+  SORT_BY_RATING,
+  FILTER_BY_SECTION,
+  FILTER_BY_CATEGORY,
+  CLEAR_FILTERS,
+} from "./constants";

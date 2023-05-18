727e83a4654bfe6e6442aa25c47f5bd5b6ef5e7a
Chhakuli <chhakulizingare12@gmail.com>
2023-05-18 15:33:02 +0530

    feat: Split filterActions for improved modularity

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -1,136 +1,45 @@
-import { products } from "backend/db/products";
-
-export const initialState = {
-  products: products,
-  sortByPrice: "",
-  sortByRating: "",
-  priceRange: { min: 0, max: 1000 },
-  selectedSections: [],
-  filteredProducts: products,
-  selectedCategory: [],
-};
+import {
+  CLEAR_FILTERS,
+  FILTER_BY_CATEGORY,
+  FILTER_BY_SECTION,
+  SEARCH_PRODUCTS,
+  SORT_BY_RATING,
+  SORT_PRODUCTS,
+  UPDATE_PRICE_RANGE,
+  UPDATE_PRODUCTS,
+  clearFilters,
+  filterByCategory,
+  filterBySection,
+  searchProducts,
+  sortByRating,
+  sortProducts,
+  updatePriceRange,
+  updateProducts,
+} from "utils";
 
 const filterReducer = (state, action) => {
   switch (action.type) {
-    case "SORT_PRODUCTS":
-      if (action.payload === "highToLow") {
-        return {
-          ...state,
-          filteredProducts: state.filteredProducts.sort(
-            (a, b) => b.price - a.price
-          ),
-        };
-      } else if (action.payload === "lowToHigh") {
-        return {
-          ...state,
-          filteredProducts: state.filteredProducts.sort(
-            (a, b) => a.price - b.price
-          ),
-        };
-      } else if (action.payload === "aToz") {
-        return {
-          ...state,
-          filteredProducts: state.filteredProducts.sort((a, b) =>
-            a.name.localeCompare(b.name)
-          ),
-        };
-      } else if (action.payload === "zToa") {
-        return {
-          ...state,
-          filteredProducts: state.filteredProducts.sort((a, b) =>
-            b.name.localeCompare(a.name)
-          ),
-        };
-      }
-      return {
-        ...state,
-        sortByPrice: action.payload,
-        filteredProducts: products,
-      };
-    case "UPDATE_PRICE_RANGE":
-      return {
-        ...state,
-        priceRange: action.payload,
-        filteredProducts: products.filter(
-          (product) =>
-            product.price >= action.payload.min &&
-            product.price <= action.payload.max
-        ),
-      };
-    case "SORT_BY_RATING":
-      const { payload } = action;
-      let sortedProducts;
+    case SEARCH_PRODUCTS:
+      return searchProducts(state, action.payload);
+    case UPDATE_PRODUCTS:
+      return updateProducts(state, action);
+    case SORT_PRODUCTS:
+      return sortProducts(state, action.payload);
 
-      if (payload === "4stars") {
-        sortedProducts = state.filteredProducts.filter(
-          (product) => product.rating >= 4
-        );
-      } else if (payload === "3stars") {
-        sortedProducts = state.filteredProducts.filter(
-          (product) => product.rating >= 3
-        );
-      } else if (payload === "2stars") {
-        sortedProducts = state.filteredProducts.filter(
-          (product) => product.rating >= 2
-        );
-      } else if (payload === "1star") {
-        sortedProducts = state.filteredProducts.filter(
-          (product) => product.rating >= 1
-        );
-      } else {
-        sortedProducts = state.filteredProducts;
-      }
+    case UPDATE_PRICE_RANGE:
+      return updatePriceRange(state, action.payload);
 
-      return {
-        ...state,
-        sortByRating: payload,
-        filteredProducts: sortedProducts,
-      };
-    case "FILTER_BY_SECTION":
-      const section = action.payload;
-      const { selectedSections } = state;
-      const updatedSections = selectedSections.includes(section)
-        ? selectedSections.filter((s) => s !== section)
-        : [...selectedSections, section];
+    case SORT_BY_RATING:
+      return sortByRating(state, action.payload);
 
-      const filteredSectionProducts =
-        updatedSections.length > 0
-          ? state.products.filter((product) =>
-              updatedSections.includes(product.section)
-            )
-          : products;
+    case FILTER_BY_SECTION:
+      return filterBySection(state, action.payload);
 
-      return {
-        ...state,
-        selectedSections: section,
-        filteredProducts: filteredSectionProducts,
-      };
-    case "FILTER_BY_CATEGORY":
-      const category = action.payload;
-      const { selectedCategory } = state;
-      const updatedCategory = selectedCategory.includes(category)
-        ? selectedCategory.filter((c) => c !== category)
-        : [...selectedCategory, category];
+    case FILTER_BY_CATEGORY:
+      return filterByCategory(state, action.payload);
 
-      const filteredCategoryProducts =
-        updatedCategory.length > 0
-          ? state.products.filter((product) =>
-              updatedCategory.includes(product.category)
-            )
-          : products;
-
-      return {
-        ...state,
-        selectedCategory: updatedCategory,
-        filteredProducts: filteredCategoryProducts,
-      };
-    case "CLEAR_FILTERS":
-      return {
-        ...initialState,
-        selectedSections: [],
-        selectedCategory: [],
-        filteredProducts: products,
-      };
+    case CLEAR_FILTERS:
+      return clearFilters(state);
 
     default:
       return state;

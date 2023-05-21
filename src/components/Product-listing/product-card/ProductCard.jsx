6fe19d4ec9642108d62fa236ca845405c0994eff
Chhakuli <chhakulizingare12@gmail.com>
2023-05-21 06:05:02 +0530

    feat: Add wishlist actions to product card  and update styles

--- src/components/Product-listing/product-card/ProductCard.jsx
+++ src/components/Product-listing/product-card/ProductCard.jsx
@@ -6,27 +6,62 @@ import {
   FavoriteBorderOutlinedIcon,
   StarRoundedIcon,
 } from "assets";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { useNavigate } from "react-router-dom";
+import { useWishlist } from "context";
+import { ADD_TO_WISHLIST, REMOVE_FROM_WISHLIST } from "utils";
+import { toast } from "react-toastify";
 
 const ProductCard = ({ product }) => {
-  const navigate=useNavigate()
+  const { wishlistState, wishlistDispatch } = useWishlist();
+  const { wishlist } = wishlistState;
+  const navigate = useNavigate();
   const {
     _id,
     name,
     rating,
     price,
     originalPrice,
-    isOutOfStock,
+    isTrending,
     description,
     image,
   } = product;
+
+  const isFavorite =
+    wishlist && wishlist.find((wishlistProduct) => wishlistProduct._id === _id);
+
+  const toggleWishlist = () => {
+    const actionType = isFavorite ? REMOVE_FROM_WISHLIST : ADD_TO_WISHLIST;
+    const payload = isFavorite ? _id : product;
+
+    wishlistDispatch({
+      type: actionType,
+      payload,
+    });
+
+    const toastMessage = isFavorite
+      ? "Removed from wishlist"
+      : "Added to wishlist";
+    toast.success(toastMessage);
+  };
   return (
     <div>
-      <div className="product-card" key={_id} onClick={()=>navigate(`/productDetails/${_id}`)} >
-        <img src={image} alt={name} />
-        {isOutOfStock && <span className="card-badge">Out of stock</span>}
-        <span className="whishlist-heart">
-          <FavoriteBorderOutlinedIcon />
+      <div className="product-card" key={_id}>
+        <img
+          src={image}
+          alt={name}
+          onClick={() => navigate(`/productDetails/${_id}`)}
+        />
+        {isTrending && <span className="card-badge">Trending</span>}
+        <span
+          className={`whishlist-heart ${isFavorite ? "favorite" : ""}`}
+          onClick={toggleWishlist}
+        >
+          {isFavorite ? (
+            <FavoriteIcon color="error" />
+          ) : (
+            <FavoriteBorderOutlinedIcon />
+          )}
         </span>
         <div className="product-card-info">
           <div className="product-card-header">
@@ -39,7 +74,7 @@ const ProductCard = ({ product }) => {
             </div>
           </div>
           <h3 className="product-card-description">{description}</h3>
-          <div className="price">
+          <div className="product-card-price">
             <p className="product-card-price">₹{price}</p>
             <p className="product-original-price">₹{originalPrice}</p>
           </div>

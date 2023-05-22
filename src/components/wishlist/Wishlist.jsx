e97759593b08aa65eed6df0c854aae164348044e
Chhakuli <chhakulizingare12@gmail.com>
2023-05-22 15:46:07 +0530

    feat: Add "Add to Cart" functionality in Wishlist and refactor css

--- src/components/wishlist/Wishlist.jsx
+++ src/components/wishlist/Wishlist.jsx
@@ -1,19 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { useCart, useWishlist } from "context";
 import {
   ShoppingCartCheckoutIcon,
   DeleteOutlinedIcon,
   AddShoppingCartRoundedIcon,
 } from "assets";
-import { useWishlist } from "context";
-import React from "react";
+import { ADD_TO_CART, REMOVE_FROM_WISHLIST } from "utils";
 
 import "./wishlist.css";
-import { useNavigate } from "react-router-dom";
-import { REMOVE_FROM_WISHLIST } from "utils";
-import { toast } from "react-toastify";
+
 const Wishlist = () => {
   const navigate = useNavigate();
   const { wishlistState, wishlistDispatch } = useWishlist();
   const { wishlist } = wishlistState;
+  const { cartState, cartDispatch } = useCart();
+  const { cart } = cartState;
+
   return (
     <div className="middle-content">
       <div className="whislist-container">
@@ -21,7 +26,7 @@ const Wishlist = () => {
         {wishlist.length === 0 ? (
           <div className="wishlist-empty-image">
             <img
-              src="https://res.cloudinary.com/dptfwcnro/image/upload/v1684491458/E-comm%20ATTIREX/Empty/empty-wishlist_vahejo.webp"
+              src="https://res.cloudinary.com/dptfwcnro/image/upload/v1684749540/E-comm%20ATTIREX/Empty/undraw_Wishlist_re_m7tv_vk76yk.png"
               alt=""
             />
             <button
@@ -34,49 +39,55 @@ const Wishlist = () => {
           </div>
         ) : (
           <div className="wishlist-items">
-            {wishlist.map(
-              (product) =>
-                product && (
-                  <div key={product._id} className="wishlist-item">
-                    <img
-                      src={product.image}
-                      alt={product.name}
-                      className="wishlist-product-image"
-                    />
-                    <div className="wishlist-item-details">
-                      <h3 className="wishlist-product-name">{product.name}</h3>
-                      <h3 className="wishlist-product-description">
-                        {product.description}
-                      </h3>
-                      <div className="wishlist-price">
-                        <p className="wishlist-product-card-price">
-                          ₹{product.price}
-                        </p>
-                        <p className="wishlist-product-original-price">
-                          ₹{product.originalPrice}
-                        </p>
-                      </div>
-                      <div className="wishlist-card-actions">
-                        <button className="wishlist-move-to-cart-btn">
-                          <ShoppingCartCheckoutIcon className="icon" /> Move to
-                          Cart
-                        </button>
-                      </div>
+            {wishlist.map((product) =>
+              product && (
+                <div key={product._id} className="wishlist-item">
+                  <img
+                    src={product.image}
+                    alt={product.name}
+                    className="wishlist-product-image"
+                  />
+                  <div className="wishlist-item-details">
+                    <h3 className="wishlist-product-name">{product.name}</h3>
+                    <h3 className="wishlist-product-description">
+                      {product.description}
+                    </h3>
+                    <div className="wishlist-price">
+                      <p className="wishlist-product-card-price">₹{product.price}</p>
+                      <p className="wishlist-product-original-price">₹{product.originalPrice}</p>
+                    </div>
+                    <div className="wishlist-card-actions">
                       <button
-                        className="wishlist-delete-icon"
+                        className="wishlist-move-to-cart-btn"
                         onClick={() => {
-                          wishlistDispatch({
-                            type: REMOVE_FROM_WISHLIST,
-                            payload: product._id,
+                          cartDispatch({
+                            type: ADD_TO_CART,
+                            payload: product,
                           });
-                          toast.success('Item removed from wishlist');
+                          navigate("/cart");
                         }}
                       >
-                        <DeleteOutlinedIcon className="delete-icon"/>
+                        <ShoppingCartCheckoutIcon className="icon" />
+                        {cart.some((item) => item._id === product._id)
+                          ? "Already In Cart"
+                          : "Add To Cart"}
                       </button>
                     </div>
+                    <button
+                      className="wishlist-delete-icon"
+                      onClick={() => {
+                        wishlistDispatch({
+                          type: REMOVE_FROM_WISHLIST,
+                          payload: product._id,
+                        });
+                        toast.success("Item removed from wishlist");
+                      }}
+                    >
+                      <DeleteOutlinedIcon className="delete-icon" />
+                    </button>
                   </div>
-                )
+                </div>
+              ) 
             )}
           </div>
         )}

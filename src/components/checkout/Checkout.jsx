4fd0882762cc87b8e32f7367dd26582611a66483
Chhakuli <chhakulizingare12@gmail.com>
2023-05-24 08:33:17 +0530

    feat: Add Checkout  component & styling

---
+++ src/components/checkout/Checkout.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import "./checkout.css";
+import { useCart } from "context";
+import { AddressSelection } from "./AddressSelection";
+import { OrderDetails } from "./OrderDetails";
+import { PriceDetails } from "./PriceDetails";
+import { DeliveryDetails } from "./DeliveryDetails ";
+import { addresses } from "./addresses";
+import { handleCheckout } from "./checkoutLogic";
+
+const Checkout = () => {
+  const { cartState } = useCart();
+  const { cart } = cartState;
+  const location = useLocation();
+  const {
+    quantity,
+    totalPrice,
+    totalMrp,
+    discountedPrice,
+    isCouponApplied,
+    coupon,
+  } = location.state;
+  
+
+  const [selectedAddress, setSelectedAddress] = useState(addresses[0]);
+
+  const handleAddressChange = (event) => {
+    const selectedId = parseInt(event.target.value);
+    const address = addresses.find((addr) => addr.id === selectedId);
+    setSelectedAddress(address);
+  };
+
+
+  const handleCheckoutClick = () => {
+    handleCheckout(selectedAddress, totalPrice);
+  };
+
+  return (
+    <>
+      <div className="checkout-heading">
+        <h2>Checkout 🎉</h2>
+      </div>
+      <div className="checkout-container">
+      <AddressSelection
+          addresses={addresses}
+          selectedAddress={selectedAddress}
+          handleAddressChange={handleAddressChange}
+        />
+
+        <div className="right-order-container">
+        <OrderDetails cart={cart} quantity={quantity} />
+
+
+        <PriceDetails
+            cart={cart}
+            totalMrp={totalMrp}
+            discountedPrice={discountedPrice}
+            isCouponApplied={isCouponApplied}
+            coupon={coupon}
+            totalPrice={totalPrice}
+          />
+
+          <DeliveryDetails selectedAddress={selectedAddress} />
+
+          <div className="place-order-container">
+            <button className="place-order-button" onClick={handleCheckoutClick}>
+              Place Order
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { Checkout };

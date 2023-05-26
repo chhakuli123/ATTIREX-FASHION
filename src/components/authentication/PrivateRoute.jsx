1132110ab11a1d81ec0a13c68060d4b9ee336161
Chhakuli <chhakulizingare12@gmail.com>
2023-05-26 14:05:41 +0530

    feat: Add Private Route

---
+++ src/components/authentication/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+import { useAuth } from "context";
+
+const PrivateRoute = ({ children }) => {
+  const location = useLocation();
+  const { isLoggedIn } = useAuth();
+  return isLoggedIn ? (
+  <Outlet/>
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace/>
+  );
+};
+
+export { PrivateRoute };

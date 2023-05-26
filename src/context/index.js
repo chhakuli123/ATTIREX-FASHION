152e2116be47a62781a00d4476462c930ab692be
Chhakuli <chhakulizingare12@gmail.com>
2023-05-26 14:12:10 +0530

    chore: Export new context

--- src/context/index.js
+++ src/context/index.js
@@ -2,3 +2,4 @@ export { useProductsData, ProductsDataProvider } from "./productsContext";
 export { useFilter, FilterContextProvider } from "./filterContext";
 export { useWishlist, WishlistContextProvider } from "./wishlistContext";
 export { useCart, CartContextProvider } from "./cartContext";
+export { useAuth, AuthProvider } from "./authContext";

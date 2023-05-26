c5e84a3c3e1e44d1df5dc26813b1f86e8be3e095
Chhakuli <chhakulizingare12@gmail.com>
2023-05-26 14:04:28 +0530

    refactor: Replace new user details

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -1,5 +1,4 @@
 import { v4 as uuid } from "uuid";
-import { formatDate } from "../utils/authUtils";
 /**
  * User Database can be added here.
  * You can add default users of your wish with different attributes
@@ -9,11 +8,9 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshbalika",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
+    firstName: "Daisy",
+    lastName: "Khurana",
+    email: "daisykhurana@gmail.com",
+    password: "Daisy@789",
   },
 ];

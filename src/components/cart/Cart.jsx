5d218e0df316f756cdadd3c61042a18b59607d3d
Chhakuli <chhakulizingare12@gmail.com>
2023-05-26 14:08:33 +0530

    refactor: Changed toast

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import { toast } from  "react-hot-toast";
 
 import "./cart.css";
 import { AddShoppingCartRoundedIcon } from "assets";

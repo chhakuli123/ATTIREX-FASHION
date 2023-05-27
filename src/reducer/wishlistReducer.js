999a56f16afe4045658b5fa31beca17ad68b8555
Chhakuli <chhakulizingare12@gmail.com>
2023-05-27 19:48:55 +0530

    feat: Add new case

--- src/reducer/wishlistReducer.js
+++ src/reducer/wishlistReducer.js
@@ -1,21 +1,24 @@
-import { ADD_TO_WISHLIST, REMOVE_FROM_WISHLIST } from "utils";
+import { ADD_TO_WISHLIST, REMOVE_FROM_WISHLIST, SET_WISHLIST } from "utils";
 
 const wishlistReducer = (state, action) => {
   switch (action.type) {
+    case SET_WISHLIST:
+      return {
+        ...state,
+        wishlist: action.payload,
+      };
+
     case ADD_TO_WISHLIST:
       const { payload } = action;
 
-      // Check if the product is already in the wishlist
       const isAlreadyInWishlist = state.wishlist.some(
         (item) => item._id === payload._id
       );
 
-      // If the item is already in the wishlist, return the current state
       if (isAlreadyInWishlist) {
         return state;
       }
 
-      // Add the item to the wishlist
       const updatedWishlist = [...state.wishlist, payload];
 
       return {
@@ -24,7 +27,6 @@ const wishlistReducer = (state, action) => {
       };
 
     case REMOVE_FROM_WISHLIST:
-      // Remove the item from the wishlist
       return {
         ...state,
         wishlist: state.wishlist.filter(

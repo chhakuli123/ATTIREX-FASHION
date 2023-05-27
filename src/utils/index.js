42ae03ed99ad244c787d494e4aa594c373fe8f50
Chhakuli <chhakulizingare12@gmail.com>
2023-05-27 19:46:09 +0530

    chore: Export of new constant & wishlistServices

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,9 +17,15 @@ export {
   FILTER_BY_SECTION,
   FILTER_BY_CATEGORY,
   CLEAR_FILTERS,
+  SET_WISHLIST,
   ADD_TO_WISHLIST,
   REMOVE_FROM_WISHLIST,
   ADD_TO_CART,
   REMOVE_FROM_CART,
 } from "./constants";
 export { loginService, signupService } from "./authServices";
+export {
+  getWishlist,
+  addToWishlist,
+  removeFromWishlist,
+} from "./wishlistServices";
